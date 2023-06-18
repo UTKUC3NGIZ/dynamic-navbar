00a035ee3bb008cffa6b28a9fb1b6685f897ac68
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-18 13:57:28 +0300

    hover olunduğunda menülerin açılması

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import { useState } from "react";
 import jsonData from "./data/data.json";
 
 function App() {
   return (
-    <nav className="px-56 py-10">
-      <ul className="flex flex-row justify-between">
+    <nav className={`px-56 py-10 ${jsonData.navbarColor}`}>
+      <ul className="flex flex-row justify-between ">
         {jsonData.buttons.map((buttons, index) => (
-          <li key={index} className={buttons.css}>
-            <a href="">{buttons.text}</a>
+          <li key={index} className="flex flex-col  group">
+            <a
+              href={buttons.link}
+              className={`hover:bg-slate-800 rounded-2xl ${buttons.css}`}
+            >
+              {buttons.text}
+            </a>
             {typeof buttons.parentButton === "object" ? (
-              <ul>
+              <ul className="hidden group-hover:flex flex-col mt-8 text-white ">
                 {buttons.parentButton.map((parentButton, index) => (
-                  <li key={index}>
-                    <a href="">{parentButton.text}</a>
+                  <li key={index} className="flex flex-col mb-5 group/childButtons">
+                    <a
+                      href="/childButtons"
+                      className="border-2 py-2 px-4 mb-2 rounded-2xl "
+                    >
+                      {parentButton.text}
+                    </a>
                     {typeof parentButton.childButtons === "object" ? (
-                      <ul>
+                      <ul className="hidden group-hover/childButtons:flex flex-col ml-12">
                         {parentButton.childButtons.map(
                           (childButtons, index) => (
-                            <li key={index}>
-                              <a href="">{childButtons.text}</a>
+                            <li
+                              key={index}
+                              className="border-2 mb-3 rounded-2xl px-4 py-2"
+                            >
+                              <a href="/deneme">{childButtons.text}</a>
                             </li>
                           )
                         )}
