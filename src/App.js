c6b61ca281ec3aca23146b307abeaa1b9dde3f69
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-19 02:42:27 +0300

    tasarım ve animasyon tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
 import jsonData from "./data/data.json";
 
 function App() {
+  function handleLinkClickMain(e, data) {
+    if (typeof data.parentButton === "object") {
+      e.preventDefault();
+    }
+  }
+  function handleLinkClickChild(e, data) {
+    if (typeof data.childButtons === "object") {
+      e.preventDefault();
+    }
+  }
   return (
-    <nav className={` my-10 w-max m-auto rounded-lg ${jsonData.navbarColor}`}>
+    <nav className={`${jsonData.navbarColor} md:w-max`}>
       <ul className="flex flex-row  ">
-        {jsonData.buttons.map((buttons, index) => (
-          <li key={index} className="flex flex-col  group relative">
+        {jsonData.buttons.map((buttons) => (
+          <li
+            key={buttons.order}
+            className="flex flex-col relative group/main "
+          >
             <a
               href={buttons.link}
-              className={`hover:bg-slate-800 text-3xl py-8 px-20 cursor-pointer  ${buttons.css}`}
+              onClick={(e) => handleLinkClickMain(e, buttons)}
+              className={buttons.css}
             >
               {buttons.text}
             </a>
             {typeof buttons.parentButton === "object" ? (
-              <ul className="hidden group-hover:flex flex-col pt-28 text-white absolute w-full text-center">
-                {buttons.parentButton.map((parentButton, index) => (
-                  <li
-                    key={index}
-                    className="flex flex-col pb-1 group/childButtons "
-                  >
-                    <a
-                      href="/childButtons"
-                      className="border-2 py-5  text-lg bg-slate-700 hover:bg-slate-600"
+              <div className="hidden group-focus-within/main:!flex delay-150">
+                <ul className="flex flex-col pt-2 text-white absolute w-full text-center">
+                  {buttons.parentButton.map((parentButton) => (
+                    <li
+                      key={parentButton.order}
+                      className={`flex flex-col pb-1 group/child `}
                     >
-                      {parentButton.text}
-                    </a>
-                    {typeof parentButton.childButtons === "object" ? (
-                      <ul className="hidden group-hover/childButtons:flex flex-col pb-2 ">
-                        {parentButton.childButtons.map(
-                          (childButtons, index) => (
-                            <li key={index} className="flex">
+                      <a
+                        onClick={(e) => handleLinkClickChild(e, parentButton)}
+                        href={parentButton.link}
+                        className={parentButton.css}
+                      >
+                        {parentButton.text}
+                      </a>
+                      {typeof parentButton.childButtons === "object" ? (
+                        <ul className="hidden group-focus-within/child:flex  flex-col pb-2 ">
+                          {parentButton.childButtons.map((childButtons) => (
+                            <li key={childButtons.order} className="flex">
                               <a
-                                href="/deneme"
-                                className="w-full text-base border-2 py-3 bg-slate-500 hover:bg-slate-400"
+                                href={childButtons.link}
+                                className={childButtons.css}
                               >
                                 {childButtons.text}
                               </a>
                             </li>
-                          )
-                        )}
-                      </ul>
-                    ) : (
-                      ""
-                    )}
-                  </li>
-                ))}
-              </ul>
+                          ))}
+                        </ul>
+                      ) : (
+                        ""
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             ) : (
               ""
             )}
